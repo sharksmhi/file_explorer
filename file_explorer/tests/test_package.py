04d5369c3db04a39b286f81dc2b000cb66aed1c8
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2022-09-20 10:08:45 +0200

    Add test nr of suffix

--- file_explorer/tests/test_package.py
+++ file_explorer/tests/test_package.py
@@ -2,6 +2,8 @@ import datetime
 import file_explorer
 from file_explorer.tests.test_data import TEST_DATA_DIR
 from file_explorer.tests.test_data import CNV_TEST_FILE
+from file_explorer.tests.test_data import LOCAL_TEST_DIR
+import pathlib
 
 
 def test_package_nr_packs():
@@ -9,14 +11,14 @@ def test_package_nr_packs():
     packs2 = file_explorer.get_packages_in_directory(TEST_DATA_DIR, with_id_as_key=True)
     packs3 = file_explorer.get_packages_in_directory(TEST_DATA_DIR, as_list=True)
 
-    assert len(packs1) == 3
-    assert len(packs2) == 2
-    assert len(packs3) == 3
+    assert len(packs1) == 5
+    assert len(packs2) == 4
+    assert len(packs3) == 5
 
 
 def test_package_nr_packs_exclude_directory():
     packs = file_explorer.get_packages_in_directory(TEST_DATA_DIR, as_list=True, exclude_directory='arkiv_2004')
-    assert len(packs) == 2
+    assert len(packs) == 4
 
 
 def test_package_id_as_key():
@@ -36,5 +38,13 @@ def test_package_no_datetime_from_file_name():
     assert pack_false.datetime == d_false
     assert pack_true.datetime == d_true
     assert pack_default.datetime == d_false
+
+
+def test_suffix_in_package():
+    packs = file_explorer.get_packages_in_directory(LOCAL_TEST_DIR)
+    pack = packs['SBE09_1387_20220823_1041_77SE_14_0600']
+    nr_suffix = len(set([file.suffix for file in pack.files]))
+    assert nr_suffix == 12
+
 
 
