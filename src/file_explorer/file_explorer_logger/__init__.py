2f0095b9885d875cbcec00b8a47c06173e80f360
Wenzer Magnus <a001985@ad.smhi.se>
2024-04-24 13:58:55 +0200

    Add filter to xlsx export

--- src/file_explorer/file_explorer_logger/__init__.py
+++ src/file_explorer/file_explorer_logger/__init__.py
@@ -2,7 +2,10 @@ from .logger import FileExplorerLogger
 from .exporter import XlsxExporter
 
 
-def create_xlsx_report(logger: FileExplorerLogger, **kwargs):
+def create_xlsx_report(logger: FileExplorerLogger, filter: dict = None, **kwargs):
+    if filter:
+        logger.reset_filter()
+        logger.filter(**filter)
     exp = XlsxExporter(**kwargs)
     logger.export(exp)
 
